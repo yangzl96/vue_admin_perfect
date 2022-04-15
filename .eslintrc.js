5111ee5cfab4028c41a0928ca45026b34835ad8e
ex-yangzl <exyangzl@szca.com>
2022-04-15 18:39:55 +0800

    feat(git配置): add commitizen
    使用commitizen来规范代码提交

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
 module.exports = {
+  // 标记当前在根目录
   root: true,
+  // 检测环境
   env: {
+    // 在node环境下启用
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  // 继承的配置
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
+  // 解析器
   parserOptions: {
     parser: 'babel-eslint'
   },
+  // off | 0  ：关闭规则
+  // warn | 1 ：开启规则，使用警告级别的错误，程序不会退出
+  // error | 2 ：开启规则，使用错误级别的错误，程序会退出
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: 'warn',
+    'space-before-function-paren': 'off'
   }
 }
