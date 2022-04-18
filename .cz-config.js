406332a71742dd53e96f8beeb515bee32740a407
ex-yangzl <exyangzl@szca.com>
2022-04-18 15:54:51 +0800

    docs: 修改空格

--- .cz-config.js
+++ .cz-config.js
@@ -10,7 +10,7 @@ module.exports = {
     { value: 'test', name: 'test:      增加测试' },
     { value: 'chore', name: 'chore:     构建过程或者辅助工具的变动' },
     { value: 'revert', name: 'revert:    回退' },
-    { value: 'build', name: 'build:      打包' }
+    { value: 'build', name: 'build:     打包' }
   ],
   // 步骤 提交代码的时候索要经历的
   messages: {
