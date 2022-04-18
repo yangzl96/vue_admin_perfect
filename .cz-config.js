d13fe94987d01de6599830e216067a975d2d379b
ex-yangzl <exyangzl@szca.com>
2022-04-18 15:54:12 +0800

    docs: 修改空格

--- .cz-config.js
+++ .cz-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   // 可选类型
   types: [
-    { value: 'feat', name: 'feat:       新功能' },
-    { value: 'fix', name: 'fix:       修复' },
-    { value: 'docs', name: 'docs:       文档变更' },
-    { value: 'style', name: 'style:       代码格式(不影响代码运行的变动)' },
-    { value: 'refactor', name: 'refactor:重构(既不是增加feature,也不是修复bug)' },
-    { value: 'perf', name: 'perf:       性能优化' },
-    { value: 'test', name: 'test:       增加测试' },
-    { value: 'chore', name: 'chore:       构建过程或者辅助工具的变动' },
-    { value: 'revert', name: 'revert:       回退' },
-    { value: 'build', name: 'build:       打包' }
+    { value: 'feat', name: 'feat:      新功能' },
+    { value: 'fix', name: 'fix:       修复' },
+    { value: 'docs', name: 'docs:      文档变更' },
+    { value: 'style', name: 'style:     代码格式(不影响代码运行的变动)' },
+    { value: 'refactor', name: 'refactor:  重构(既不是增加feature,也不是修复bug)' },
+    { value: 'perf', name: 'perf:      性能优化' },
+    { value: 'test', name: 'test:      增加测试' },
+    { value: 'chore', name: 'chore:     构建过程或者辅助工具的变动' },
+    { value: 'revert', name: 'revert:    回退' },
+    { value: 'build', name: 'build:      打包' }
   ],
   // 步骤 提交代码的时候索要经历的
   messages: {
@@ -21,6 +21,7 @@ module.exports = {
     footer: '请输入要关闭的issue（可选）',
     confirmCommit: '确认要使用以上信息提交？(y/n)'
   },
+  skipQuestions: ['body', 'footer'],
   // 长度限制，默认72
   subjectLimit: 72
 }
