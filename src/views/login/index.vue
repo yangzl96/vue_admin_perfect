afac3b0c8336060340eae7ae20733e996721559b
ex-yangzl <exyangzl@szca.com>
2022-04-20 10:31:37 +0800

    style(login): 完善登录页面样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="login-container">
-    <el-form class="login-form" ref="form" :model="form" label-width="80px">
+    <el-form class="login-form" ref="form" :model="form">
       <div class="title-container">
         <h3 class="title">用户登录</h3>
       </div>
-      <el-form-item label="label">
+      <el-form-item>
         <span class="svg-container">
           <el-icon>
             <avatar />
@@ -16,7 +16,7 @@
           v-model="model"
         ></el-input>
       </el-form-item>
-      <el-form-item label="label">
+      <el-form-item>
         <span class="svg-container">
           <el-icon>
             <avatar />
@@ -44,4 +44,74 @@
 import { Avatar } from '@element-plus/icons'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+$cursor: #fff;
+
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background-color: $bg;
+  overflow: hidden;
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+
+    ::v-deep .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background-color: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+
+    ::v-deep .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+
+      input {
+        background-color: transparent;
+        border: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        caret-color: $cursor;
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0 auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+}
+</style>
