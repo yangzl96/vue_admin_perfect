09bd264b5cd2c0f899c54cad9bc52031b7edb05e
ex-yangzl <exyangzl@szca.com>
2022-04-21 16:13:09 +0800

    feat: 记录登录时间

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,18 +1,23 @@
-import { login } from '@/api/sys'
+import { login, getUserInfo } from '@/api/sys'
 import md5 from 'md5'
-import { setItem, getItem } from '@/utils/storage'
+import { setItem, getItem, removeAllItem } from '@/utils/storage'
 import { TOKEN } from '@/constant'
+import { setTimeStamp } from '@/utils/auth'
 import router from '@/router'
 
 export default {
   namespaced: true,
   state: () => ({
-    token: getItem(TOKEN) || ''
+    token: getItem(TOKEN) || '',
+    userInfo: {}
   }),
   mutations: {
     setToken(state, token) {
       state.token = token
       setItem(TOKEN, token)
+    },
+    setUserInfo(state, userInfo) {
+      state.userInfo = userInfo
     }
   },
   actions: {
@@ -26,11 +31,25 @@ export default {
         }).then(data => {
           this.commit('user/setToken', data)
           router.push('/')
+          // 保存当前登录时间
+          setTimeStamp()
           resolve(data)
         }).catch(err => {
           reject(err)
         })
       })
+    },
+    // 获取用户信息
+    async getUserInfo(context) {
+      const res = await getUserInfo()
+      this.commit('user/setUserInfo', res)
+      return res
+    },
+    logout() {
+      this.commit('user/setToken', '')
+      this.commit('user/setUserInfo', {})
+      removeAllItem()
+      router.push('/login')
     }
   }
 }
