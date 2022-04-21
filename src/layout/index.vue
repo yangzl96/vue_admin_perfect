8a582c4000d80d1122fbf2bbe9b39058a4cca3ef
ex-yangzl <exyangzl@szca.com>
2022-04-21 16:12:41 +0800

    feat: layout组件页面

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,7 +1,44 @@
 <template>
-  <div></div>
+  <div class="app-wrapper">
+    <!-- 左侧 menu -->
+    <sidebar
+      class="sidebar-container"
+      :style="{ backgroundColor: variables.menuBg }"
+    ></sidebar>
+    <div class="main-container">
+      <div class="fixed-header">
+        <!-- 顶部 navbar -->
+        <navbar />
+      </div>
+      <!-- 内容区 -->
+      <app-main></app-main>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import AppMain from './components/AppMain'
+import variables from '@/styles/variables.scss'
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '~@/styles/mixin.scss';
+@import '~@/styles/variables.scss';
+
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+}
+</style>
