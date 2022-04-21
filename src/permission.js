80e141096fa5b824c4143e901c5177580b7bd613
ex-yangzl <exyangzl@szca.com>
2022-04-21 16:11:58 +0800

    feat: 路由守卫获取userInfo

--- src/permission.js
+++ src/permission.js
@@ -5,13 +5,18 @@ import store from '@/store'
 const whiteList = ['/login']
 
 // 路由前置守卫
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   // 1. 已登录，不允许进入 login
   if (store.getters.token) {
     if (to.path === '/login') {
       // 回到首页
       next('/')
     } else {
+      // 判断用户信息是否存在
+      // 不存在则获取
+      if (!store.getters.hasUserInfo) {
+        await store.dispatch('user/getUserInfo')
+      }
       // 进入下一个路由钩子函数
       next()
     }
