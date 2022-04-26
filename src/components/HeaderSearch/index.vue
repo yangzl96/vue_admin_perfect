7b80bef2dc1d185a850c40cd0f8a607d4265f96c
ex-yangzl <exyangzl@szca.com>
2022-04-26 09:44:16 +0800

    feat: 完善搜索细节、国际化

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -26,8 +26,9 @@
 
 <script setup>
 import Fuse from 'fuse.js'
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { filterRouters } from '@/utils/route'
+import { watchSwitchLang } from '@/utils/i18n'
 import { generateRoutes } from './FuseData'
 import { useRouter } from 'vue-router'
 
@@ -55,33 +56,66 @@ const querySearch = (query) => {
 // 选中回调
 const onSelectChange = (val) => {
   router.push(val.path)
+  search.value = ''
 }
 
 // 检索数据源
 const router = useRouter()
-const searchPool = computed(() => {
+let searchPool = computed(() => {
   const filterRoutes = filterRouters(router.getRoutes())
   return generateRoutes(filterRoutes)
 })
 
-const fuse = new Fuse(searchPool.value, {
-  // 是否按优先级进行排序
-  shouldSort: true,
-  // 匹配长度超过这个值的才会被认为是匹配的
-  minMatchCharLength: 1,
-  // 将被搜索的键列表。 这支持嵌套路径、加权搜索、在字符串和对象数组中搜索。
-  // name：搜索的键
-  // weight：对应的权重
-  keys: [
-    {
-      name: 'title',
-      weight: 0.7
-    },
-    {
-      name: 'path',
-      weight: 0.3
-    }
-  ]
+// 搜索库
+let fuse
+const initFuse = (searchPool) => {
+  fuse = new Fuse(searchPool, {
+    // 是否按优先级进行排序
+    shouldSort: true,
+    // 匹配长度超过这个值的才会被认为是匹配的
+    minMatchCharLength: 1,
+    // 将被搜索的键列表。 这支持嵌套路径、加权搜索、在字符串和对象数组中搜索。
+    // name：搜索的键
+    // weight：对应的权重
+    keys: [
+      {
+        name: 'title',
+        weight: 0.7
+      },
+      {
+        name: 'path',
+        weight: 0.3
+      }
+    ]
+  })
+}
+// 初始化
+initFuse(searchPool.value)
+
+// 关闭search
+const onClose = () => {
+  headerSearchSelectRef.value.blur()
+  isShow.value = false
+  searchOptions.value = []
+}
+
+watch(isShow, (val) => {
+  if (val) {
+    document.body.addEventListener('click', onClose)
+  } else {
+    document.removeEventListener('click', onClose)
+  }
+})
+
+// 处理国际化：监听语言变化，重新计算数据源初始化 `fuse` 即可
+// 执行这个函数 开启 watch监听
+watchSwitchLang(() => {
+  // 语言变化的时候会执行
+  searchPool = computed(() => {
+    const filterRoutes = filterRouters(router.getRoutes())
+    return generateRoutes(filterRoutes)
+  })
+  initFuse(searchPool.value)
 })
 </script>
 
--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,5 +1,16 @@
 import i18n from '@/i18n'
+import store from '@/store'
+import { watch } from 'vue'
 
 export function generateTitle(title) {
   return i18n.global.t('msg.route.' + title)
+}
+
+export function watchSwitchLang(...cbs) {
+  watch(
+    () => store.getters.language,
+    () => {
+      cbs.forEach(cb => cb(store.getters.language))
+    }
+  )
 }
